5ab640fa721ad25b59adcecbcf89ef763ca21cc5
TechmoNoway <cuty0406@outlook.com>
2025-01-27 12:00:00 +0700

    Create base source for new ui

---
+++ new-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button';
+
+function App() {
+    return (
+        <>
+            <div>
+                <h1 className="text-red-500">Hello world</h1>
+                <Button>Click me</Button>
+            </div>
+        </>
+    );
+}
+
+export default App;
